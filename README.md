2041caa589ba8d0984057898945c014a21987bd0
NikoTrubetzkoy <elihougadol@gmail.com>
2024-10-28 12:17:42 +0100

    Added descrition and comments /LL

---
+++ README.md
@@ -0,0 +1,11 @@
+# Présentation du site "Movies and Cast"
+
+La page propose une recherche de films par mots-clé du titre et permet de sélectionner
+l'élement des résultats dont on veut examiner la distribution.
+La liste des résultats s'affiche au bas de la page à la suite du champ de recherche.
+L'affichage de la distribution se fait dans une fenêtre modale qui s'ouvre avec le clic
+d'un bouton "See the cast".
+
+La page fait appel à l'API dédiée du site TMDB ('The Movie Database', "https://www.themoviedb.org/").
+Elle est disponible pour observation ou commentaire en ligne à l'adresse suivante :
+"https://projet-js-omega.vercel.app/".
--- script4.js
+++ script4.js
@@ -1,9 +1,10 @@
-// basename: script3.js
+// basename: script4.js
 /* Description: requêtes API sur TMDB.com
-*/
+ */
 
 "use strict";
 
+// Identifiants de connexion à l'API
 const myOptions = {
   method: "GET",
   headers: {
@@ -13,26 +14,20 @@ const myOptions = {
   },
 };
 
-const inputButton = document.querySelector(".button");
-const inputTag = document.getElementById("movieName");
+const inputButton = document.querySelector(".button"); // bouton pour lancer la recherche
+const inputTag = document.getElementById("movieName"); // champ de saisie de la requête
 const movieName = inputTag.value;
 const containerNode = document.querySelector(".container");
 //console.log(movieName);
 
-//
-async function getJSON(
-  url,
-  options,
-  errorMsg = "Something went wrong..."
-) {
+// Fonction pour interroger l'API et récupérer les résultats
+async function getJSON(url, options, errorMsg = "Something went wrong...") {
   try {
     const response = await fetch(url, options);
     if (!response.ok) {
       throw new Error(errorMsg);
     }
     const APIData = await response.json();
-    //console.log(APIData, "step 1");
-    //console.log(APIData.results, "results/step1");
     return APIData;
   } catch (APIError) {
     console.error(APIError);
@@ -40,47 +35,49 @@ async function getJSON(
   }
 }
 
+// Fonction de requête secondaire pour afficher la distribution dans la fenêtre modale
 async function getMovieCredits(myID) {
   const movieDetails = await getJSON(
     `https://api.themoviedb.org/3/movie/${myID}?append_to_response=credits&language=en-US`,
     myOptions
   );
-  console.log(movieDetails, "step3");
   const castDetails = movieDetails.credits.cast;
   //console.log(castDetails);
   const modalDiv = document.getElementById(`mdl${myID}`);
-  modalDiv.style.display='block';
-  const modalContent = modalDiv.querySelector('.modal-content');
+  modalDiv.style.display = "block";
+  const modalContent = modalDiv.querySelector(".modal-content");
   for (let actor of castDetails) {
     //console.log(actor.name, actor.character);
     const actorName = actor.name;
     const characterName = actor.character;
     const actorNode = document.createElement("span");
-    actorNode.setAttribute( 'class', 'actor')
+    actorNode.setAttribute("class", "actor");
     actorNode.innerHTML = `${actorName} played as ${characterName}<br/>`;
     modalContent.appendChild(actorNode);
   }
-  window.onclick = function(event) {
+  // fermer la fenêtre modale (1)
+  window.onclick = function (event) {
     if (event.target == modalDiv) {
       modalDiv.style.display = "none";
-      const modalSpans= modalContent.querySelector('.actor');
+      const modalSpans = modalContent.querySelector(".actor");
       modalContent.removeChild(modalSpans);
     }
-  }
-  const closeSpan= modalContent.querySelector(".close");
-  closeSpan.onclick = function(event) {
+  };
+  // fermer la fenêtre modale (2)
+  const closeSpan = modalContent.querySelector(".close");
+  closeSpan.onclick = function (event) {
     if (event.target == closeSpan) {
       modalDiv.style.display = "none";
-      const modalSpans= modalContent.querySelector('.actor');
+      const modalSpans = modalContent.querySelector(".actor");
       modalContent.removeChild(modalSpans);
     }
-  }
+  };
 }
 
+// Fonction de génération et affichage de la recherche de films
 async function insertMovieData(myName) {
-  const previousQuery = containerNode.querySelectorAll('.movie');
-  console.log(previousQuery, typeof previousQuery);
-
+  const previousQuery = containerNode.querySelectorAll(".movie");
+  //console.log(previousQuery, typeof previousQuery);
   for (let previousMovie of previousQuery) {
     containerNode.removeChild(previousMovie);
   }
@@ -88,11 +85,13 @@ async function insertMovieData(myName) {
     `https://api.themoviedb.org/3/search/movie?query=${myName}`,
     myOptions
   );
-  console.log(myResponse.results);
+  //console.log(myResponse.results);
+  // Boucle sur chaque film des résultats
   for (let movie of myResponse.results) {
     const movieID = movie.id;
     const poster = movie.poster_path;
     //console.log(poster);
+    // Affichage des vignettes de film
     const movieElement = `
     <div id=${movieID} class="movie">
       <img src="https://media.themoviedb.org/t/p/w220_and_h330_face${poster}" alt='poster_film' />
@@ -110,12 +109,14 @@ async function insertMovieData(myName) {
     </div>`;
     containerNode.insertAdjacentHTML("beforeend", movieElement);
     //const movieDiv = document.getElementById(movieID);
+    // Bouton pour afficher la fenêtre modale
     document.getElementById(`btn${movieID}`).onclick = async () => {
       getMovieCredits(movieID);
-    }
+    };
   }
 }
 
+// ---- MAIN ----
 inputButton.addEventListener("click", () => {
   const movieName = inputTag.value;
   console.log(movieName);
